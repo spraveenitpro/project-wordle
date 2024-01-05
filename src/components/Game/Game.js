bc475b16d22681927e57f5beba20701cdd687528
Praveen <spraveenitpro@gmail.com>
2024-01-04 22:50:44 -0800

    Implemented slot boxes feature

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,7 +8,7 @@ import Guess from '../Guess';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
+
 console.info({ answer });
 
 function Game() {
